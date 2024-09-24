27fe16b9d2872f46a1fe4db06b59445a95578745
maria.kudinova <maria.kudinova@office.e2e4gu.ru>
2024-09-24 14:38:47 +0700

    update nginx config and client files

--- details/script.js
+++ details/script.js
@@ -3,22 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => response.json())
     .then(data => {
         const ip = data.ip;
-        const screenWidth = screen.width ? screen.width : '';
-        //const screenHeight = screen.height ? screen.height : '';
+        const screenWidth = window.screen.width ? window.screen.width : '';
 
         const os = platform.os.family.toLowerCase();
+        const version = platform.os.version;
 
         var widthEl = document.getElementById('width-p');
         widthEl.innerHTML += screenWidth;
-
-        //var heightEl = document.getElementById('height-p');
-        //heightEl.innerHTML += screenHeight;
 
         var ipEl = document.getElementById('ip-p');
         ipEl.innerHTML += ip;
 
         var osEl = document.getElementById('os-p');
         osEl.innerHTML += os;
+
+        var versionEl = document.getElementById('version-p');
+        versionEl.innerHTML += version;
+
+        console.log(window.location.href);
     })
     .catch(error => {
         console.log('Error:', error);
