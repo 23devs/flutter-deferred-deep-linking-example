472e0b7b301717eacdc8493d1e63739ef257c1f9
maria.kudinova <maria.kudinova@office.e2e4gu.ru>
2024-09-24 21:10:21 +0700

    update: get data on client

--- details/script.js
+++ details/script.js
@@ -1,45 +1,48 @@
-document.addEventListener(
+window.addEventListener(
     "DOMContentLoaded",
-    async () => {
-        const screenWidth = window.screen.width ? window.screen.width : "";
-
-        const os = platform.os.family.toLowerCase();
-        const version = platform.os.version;
-
-        var widthEl = document.getElementById("width-p");
-        widthEl.innerHTML += screenWidth;
-
-        var osEl = document.getElementById("os-p");
-        osEl.innerHTML += os;
-
-        var versionEl = document.getElementById("version-p");
-        versionEl.innerHTML += version;
-
-        console.log(window.location.href);
-
-        const data = {
-            screenWidth,
-            os,
-            version,
-            url: window.location.href.replace(
-                "https://mobile-apps-examples.23devs.com",
-                ""
-            ),
-        };
-
-        let response = await fetch(
-            "https://api-mobile-apps-examples.23devs.com/api/url-access-datas/set",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;charset=utf-8",
-                },
-                body: JSON.stringify(data),
-            }
-        );
-
-        let result = await response.json();
-        console.log(result);
-    },
-    false
+    load,
+    { once: true }
 );
+
+async function load() {
+    const screenWidth = window.screen.width ? window.screen.width : "";
+
+    const os = platform.os.family.toLowerCase();
+    const version = platform.os.version;
+
+    var widthEl = document.getElementById("width-p");
+    widthEl.innerHTML += screenWidth;
+
+    var osEl = document.getElementById("os-p");
+    osEl.innerHTML += os;
+
+    var versionEl = document.getElementById("version-p");
+    versionEl.innerHTML += version;
+
+    const data = {
+        screenWidth,
+        os,
+        version,
+        url: window.location.href.replace(
+            "https://mobile-apps-examples.23devs.com",
+            ""
+        ),
+    };
+
+    let response = await fetch(
+        "http://api-mobile-apps-examples.23devs.com/api/url-access-datas/set",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(data),
+        }
+    );
+
+    let result = await response.json();
+
+    if (result.redirectUrl) {
+        window.open(result.redirectUrl);
+    }
+}
