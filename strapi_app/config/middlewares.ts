e260e9ddaa38422570fa94e73868b5d5955bee70
maria.kudinova <maria.kudinova@office.e2e4gu.ru>
2024-09-25 17:06:51 +0700

    update middlewares for cors and env vars

--- strapi_app/config/middlewares.ts
+++ strapi_app/config/middlewares.ts
@@ -3,6 +3,18 @@ export default [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        process.env.CLIENT_URL,
+        process.env.API_URL,
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
