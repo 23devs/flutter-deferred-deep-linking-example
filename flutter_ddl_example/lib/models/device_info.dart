0be5ed44162c72784170263691c5724bcdf07fb0
maria.kudinova <maria.kudinova@office.e2e4gu.ru>
2024-09-26 13:13:10 +0700

    remove get ip from device

--- flutter_ddl_example/lib/models/device_info.dart
+++ flutter_ddl_example/lib/models/device_info.dart
@@ -1,44 +1,26 @@
 import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:public_ip_address/public_ip_address.dart';
 
 class DeviceInfo {
   int? deviceWidth;
-  String? ip;
   String? os;
 
   DeviceInfo({
     this.deviceWidth,
-    this.ip,
     this.os,
   });
 
   Map<String, dynamic> toJson() => {
         'deviceWidth': deviceWidth,
-        // 'deviceHeight': deviceHeight,
-        'ip': ip,
         'os': os,
       };
 
   int getDeviceWidth() {
     FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-    // Dimensions in logical pixels (dp)
     Size size = view.physicalSize;
     int width = (size.width / view.devicePixelRatio).toInt();
     return width;
-  }
-
-  // int getDeviceHeight() {
-  //   FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-  //   // Dimensions in logical pixels (dp)
-  //   Size size = view.physicalSize;
-  //   int height = (size.height / view.devicePixelRatio).toInt();
-  //   return height;
-  // }
-
-  Future<String> getPublicIP() async {
-    return await IpAddress().getIp();
   }
 
   String getDeviceOS() {
@@ -48,13 +30,8 @@ class DeviceInfo {
     return 'ios';
   }
 
-  String getTimeStamp() {
-    return DateTime.now().toUtc().toIso8601String();
-  }
-
   Future<void> setDeviceInfo() async {
     deviceWidth = getDeviceWidth();
     os = getDeviceOS();
-    ip = await getPublicIP();
   }
 }
